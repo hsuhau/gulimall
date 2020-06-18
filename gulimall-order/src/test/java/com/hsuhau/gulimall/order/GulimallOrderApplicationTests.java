c2d8d98e053326959ed49e3ea2573862e93aabe2
hsuhau <hsuhau@foxmail.com>
2020-06-18 15:10:44 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/hsuhau/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsuhau.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/test/java/com/hsuhau/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsuhau.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
